c92b8247f980cc42e64a7c20b2967e8fd075d82e
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-22 15:29:14 -0400

    complete exercise 9

--- exercises.py
+++ exercises.py
@@ -146,4 +146,32 @@ def product(*args):
     return product
 
 
-print("Exercise 8:", product(-1, 4))
+# print("Exercise 8:", product(-1, 4))
+
+# Exercise 9: Basic Calculator
+
+# Create a function named basicCalculator that takes three arguments:
+# two numbers and a string representing an operation (‘add’, ‘subtract’, ‘multiply’, ‘divide’).
+# Perform the provided operation on the two numbers. In operations where the order of numbers is important,
+# treat the first parameter as the first operand and the second parameter as the second operand.
+
+# Examples:
+# basicCalculator(10, 5, ‘subtract’) should return 5.
+# basicCalculator(10, 5, ‘add’) should return 15.
+# basicCalculator(10, 5, ‘multiply’) should return 50.
+# basicCalculator(10, 5, ‘divide’) should return 2.
+
+
+# Define the function and then call it below.
+def basicCalculator(n1, n2, operation):
+    if operation == "subtract":
+        return n1 - n2
+    elif operation == "add":
+        return n1 + n2
+    elif operation == "multiply":
+        return n1 * n2
+    else:
+        return n1 / n2
+
+
+print("Exercise 9 Result:", basicCalculator(10, 5, "divide"))
